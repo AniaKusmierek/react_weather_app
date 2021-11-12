e25d1a56e7e73714f1c126511eaf7c67d11862f0
Anna Kusmierek <aniak1100@gmail.com>
2021-11-12 20:07:44 +0000

    Added the state values

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,20 @@ import "./App.css";
 import Form from "./Form";
 import Result from "./Result";
 
+const keyAPI = process.env.REACT_APP_WEATHER_API_KEY;
+
 class App extends Component {
+  state = {
+    value: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    pressure: "",
+    temp: "",
+    wind: "",
+    err: false,
+  };
+
   render() {
     return (
       <div className="App">
