f37a895d0a02a4ea5f0bb894d1a27c8370057d16
Anna Kusmierek <aniak1100@gmail.com>
2021-11-12 20:53:55 +0000

    Pass variables and props to Result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,44 @@
 import React from "react";
 
-const Result = () => {
+const Result = (props) => {
+  const { city, date, sunrise, sunset, temp, pressure, wind, err } =
+    props.weather;
+  let content = null;
+
+  if (!err && city) {
+    const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
+    const sunsetTime = new Date(sunset * 1000).toLocaleTimeString();
+    content = (
+      <div>
+        <h1>
+          The weather for <em>{city}</em>:
+        </h1>
+        <p>
+          Today is <strong>{date}</strong>
+        </p>
+        <p>
+          Sunrise at <strong>{sunriseTime}</strong>
+        </p>
+        <p>
+          Sunset at <strong>{sunsetTime}</strong>
+        </p>
+        <p>
+          Temperature is <strong>{temp} °C</strong>
+        </p>
+        <p>
+          Pressure is <strong>{pressure} hPa</strong>
+        </p>
+        <p>
+          Wind speed is <strong>{wind} m/s</strong>
+        </p>
+      </div>
+    );
+  }
   return (
     <>
-      <h1>Result</h1>
+      <div className="result">
+        {err ? `We couldn't find ${city}...` : content}
+      </div>
     </>
   );
 };
