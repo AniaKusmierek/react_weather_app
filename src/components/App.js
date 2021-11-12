ba338847c39dd38081fff9fac3afe909d5acec4f
Anna Kusmierek <aniak1100@gmail.com>
2021-11-12 20:39:11 +0000

    Created handleCitySubmit function

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,40 @@ class App extends Component {
       value: e.target.value,
     });
   };
+  handleCitySubmit = (e) => {
+    e.preventDefault();
+
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${keyAPI}&units=metric`;
+    fetch(API)
+      .then((res) => {
+        if (res.ok) {
+          return res;
+        }
+        throw Error("Cannot find the town provided...");
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        const time = new Date().toLocaleString();
+        this.setState((prevState) => ({
+          err: false,
+          date: time,
+          city: prevState.value,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          pressure: data.main.pressure,
+          temp: data.main.temp,
+          wind: data.wind.speed,
+        }));
+      })
+      .catch((error) => {
+        this.setState((prevState) => {
+          return {
+            err: true,
+            city: prevState.value,
+          };
+        });
+      });
+  };
 
   render() {
     return (
